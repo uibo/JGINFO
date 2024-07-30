f3646b985e26fb4847f73e04c145653cdaa68c3a
uibo <boui000210@gmail.com>
2024-07-30 14:46:58 +0900

    working

--- acquisition/joongnaSite_acq_data.py
+++ acquisition/joongnaSite_acq_data.py
@@ -24,60 +24,60 @@ chrome_options.add_argument("--log-level=3") # 로그 수준을 낮춰 warning m
 def get_postInfo(Urls: pd.DataFrame):
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=chrome_options)
+    engine = create_engine("mysql+pymysql://admin:12312312@ls-0417629e59c83e2cfae4e2aac001b7eee2799e0e.cxiwwsmmq2ua.ap-northeast-2.rds.amazonaws.com/joonggoinfo")
+    engine.connect()
 
-    post_df = pd.DataFrame(columns=['title', 'context', 'price', 'upload_date', 'location', 'status', 'img_url', 'url'])
-
-    i = 0
+    i = 2400
     while i < len(Urls):
-        url = "https://web.joongna.com/product/" + str(Urls.iloc[i][1])
-        driver.get(url)
-        # article element 로드 될때까지 기다림
-        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, "article")))
-        
-        # 거래희망지역 기준으로 context와 location분할
-        context = driver.find_element(By.TAG_NAME, 'article').text
-        if "거래희망지역" in context:
-            context = context.split("거래희망지역")
-            location = context[1]
-            context = context[0]
-        else:
-            location = None
-
-        # post_info 추출
-        title_tag = driver.find_element(By.TAG_NAME, 'h1')
-        complex = driver.execute_script("return arguments[0].parentNode;", title_tag)
-        complex = driver.execute_script("return arguments[0].parentNode;", complex)
-        complex = complex.text.split('\n')
-        title = complex[0]
-        price = int(complex[1].replace("원", '').replace(",", ''))
-        upload_date = str(complex[2][:(complex[2].find('·'))-1].strip())
-        imgUrl = driver.find_element(By.CLASS_NAME, 'col-span-1').find_element(By.TAG_NAME, 'img').get_attribute("src")
-        ago = upload_date[-3:]
-        now = datetime.now()
-        if ago == "일 전":
-            n = int(upload_date[:-3])
-            upload_date = (now - relativedelta(days=n)).date().strftime('%Y.%m.%d')
-        else:
-            upload_date = upload_date.replace('-', '.')
-        # post_info를 하나의 sample로 변경
         try:
-            status = driver.find_element(By.CLASS_NAME, 'col-span-1').find_element(By.CLASS_NAME, 'absolute').text
-            if status == '판매완료' :
-                status = 1
+            url = "https://web.joongna.com/product/" + str(Urls.iloc[i, 1])
+            driver.get(url)
+            # article element 로드 될때까지 기다림
+            WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, "article")))
+            
+            # 거래희망지역 기준으로 context와 location분할
+            context = driver.find_element(By.TAG_NAME, 'article').text
+            if "거래희망지역" in context:
+                context = context.split("거래희망지역")
+                location = context[1]
+                context = context[0]
             else:
-                status = 0
-        except:
-            status = 0
-        sample = pd.DataFrame([{'id': Urls[i][0] ,'title': title, 'context':context, 'price':price, 'upload_date':upload_date, 'location':location, 'status':status, 'img_url':imgUrl}])
-        post_df = pd.concat([post_df, sample], ignore_index=True)
+                location = None
 
-        # post_info가 정상적으로 추출됨을 알림
-        i += 1
-        print(i, flush=True)
-        if (i // 100) == 0 :
-            engine = create_engine("mysql+pymysql://admin:12312312@ls-0417629e59c83e2cfae4e2aac001b7eee2799e0e.cxiwwsmmq2ua.ap-northeast-2.rds.amazonaws.com/joonggoinfo")
-            engine.connect()
-            post_df.to_sql(name="Iphone14", con=engine, if_exists='append', index=False)
+            # post_info 추출
+            title_tag = driver.find_element(By.TAG_NAME, 'h1')
+            complex = driver.execute_script("return arguments[0].parentNode;", title_tag)
+            complex = driver.execute_script("return arguments[0].parentNode;", complex)
+            complex = complex.text.split('\n')
+            title = complex[0]
+            price = int(complex[1].replace("원", '').replace(",", ''))
+            upload_date = str(complex[2][:(complex[2].find('·'))-1].strip())
+            imgUrl = driver.find_element(By.CLASS_NAME, 'col-span-1').find_element(By.TAG_NAME, 'img').get_attribute("src")
+            ago = upload_date[-3:]
+            if ago == '분 전':
+                upload_date = datetime.now().strftime('%Y.%m.%d')
+            elif ago == "일 전":
+                n = int(upload_date[:-3])
+                upload_date = (datetime.now() - relativedelta(days=n)).date().strftime('%Y.%m.%d')
+            else:
+                upload_date = upload_date.replace('-', '.')
+            # post_info를 하나의 sample로 변경
+            try:
+                status = driver.find_element(By.CLASS_NAME, 'col-span-1').find_element(By.CLASS_NAME, 'absolute').text
+                if status == '판매완료' :
+                    status = 1
+                else:
+                    status = 0
+            except:
+                status = 0
+            sample = pd.DataFrame([{'id': Urls.iloc[i, 0] ,'title': title, 'context':context, 'price':price, 'upload_date':upload_date, 'location':location, 'status':status, 'img_url':imgUrl}])
+            
+            sample.to_sql(name="iPhone14", con=engine, if_exists='append', index=False)
+
+            # post_info가 정상적으로 추출됨을 알림
+            print(i, flush=True)
+        finally:
+            i += 1
 
     
 
